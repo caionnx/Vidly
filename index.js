19038b9132d30c5f9d46daee68158d875b238f72
Caio Navarro <caionavarro.n@hotmail.com>
2019-07-15 13:53:30 +1200

    Add morgan middleware to improve development experience

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const morgan = require('morgan');
 const genresRouter = require('./routes/genres');
 
 const app = express();
 const PORT = process.env.port || 3000;
+const isDevEnv = app.get('env') === 'development';
 
 app.listen(PORT, () => `Vidly running on ${PORT}`);
 
 // Middleware
+if(isDevEnv) app.use(morgan('dev'));
 app.use(express.json());
 
 // Routes
