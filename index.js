3058855c379f91a1687c53a17155d49c3362918e
Caio Navarro <caionavarro.n@hotmail.com>
2019-07-16 15:09:02 +1200

    Change index to connect to Mongo and then listen to Port

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 const genresRouter = require('./routes/genres');
 
 const app = express();
 const PORT = process.env.port || 3000;
 const isDevEnv = app.get('env') === 'development';
 
-app.listen(PORT, () => `Vidly running on ${PORT}`);
+async function startApplication() {
+  try {
+    await mongoose.connect('mongodb://localhost/vidly', { useNewUrlParser: true }) 
+    app.listen(PORT, () => `Vidly running on ${PORT}`);
+  } catch (error) {
+    const errorMessage = error.message || 'Error while connecting to database.';
+    return console.log(errorMessage);
+  }
 
-// Middleware
-if(isDevEnv) app.use(morgan('dev'));
-app.use(express.json());
+  // Middleware
+  if(isDevEnv) app.use(morgan('dev'));
+  app.use(express.json());
 
-// Routes
-app.use('/api/genres', genresRouter);
+  // Routes
+  app.use('/api/genres', genresRouter);
+}
+
+startApplication();
